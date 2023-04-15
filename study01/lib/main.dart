375cc1c3d9ef11889f067024b0b7ca1914e1ee6c
deveb1479@gmail.com <deveb1479@gmail.com>
2023-04-16 01:39:25 +0900

    first commit

---
+++ study01/lib/main.dart
@@ -0,0 +1,167 @@
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:filesystem_picker/filesystem_picker.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_file_dialog/flutter_file_dialog.dart';
+
+void main() {
+  runApp(const MemoApp());
+}
+
+class MemoApp extends StatelessWidget {
+  const MemoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Memo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MainPage(title: 'Memo'),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MainPage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MainPage> {
+  TextEditingController textController = TextEditingController();
+  TextEditingController pathController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(5),
+            child: Text("File name to save"),
+          ),
+          Padding(
+            padding: EdgeInsets.all(5),
+            child: TextField(
+              controller: pathController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter file name',
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(5),
+            child: Text("Text data"),
+          ),
+          Padding(
+            padding: EdgeInsets.all(5),
+            child: TextFormField(
+              controller: textController,
+              decoration: InputDecoration(
+                border: UnderlineInputBorder(),
+                hintText: 'Enter text',
+              ),
+              maxLines: 20,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextButton(
+                child: Text('Load'),
+                onPressed: load,
+              ),
+              TextButton(
+                child: Text('Save'),
+                onPressed: save,
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  String getPath(String path) {
+    List<String> split;
+    if (path.split('/').length != 1) {
+      split = path.split('/');
+      return split[split.length - 1].split('.')[0];
+    } else if (path.split('\\').length != 1) {
+      split = path.split('\\');
+      return split[split.length - 1].split('.')[0];
+    } else {
+      return "ERROR";
+    }
+  }
+
+  String getSpliter(String path) {
+    if (path.split('/').length != 1) {
+      return ('/');
+    } else if (path.split('\\').length != 1) {
+      return ('\\');
+    } else {
+      return ".";
+    }
+  }
+
+  void showSnackBar(BuildContext context, String text) {
+    SnackBar snackBar = SnackBar(
+      content: Text(text),
+      duration: Duration(seconds: 4), //default is 4s
+    );
+    // Find the Scaffold in the widget tree and use it to show a SnackBar.
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  Future<void> load() async {
+    String? filePath = await FilesystemPicker.open(
+      title: 'Open file',
+      context: context,
+      rootDirectory: Directory.current,
+      allowedExtensions: ['.txt'],
+      fileTileSelectMode: FileTileSelectMode.wholeTile,
+    );
+
+    if (filePath != null) {
+      pathController.text = getPath(filePath);
+      textController.text = await File(filePath).readAsString();
+    }
+  }
+
+  Future<void> save() async {
+    if (pathController.text == "") {
+      showSnackBar(context, "Please input file name first!");
+      return;
+    }
+
+    String? folderPath = await FilesystemPicker.open(
+      title: 'Save file',
+      context: context,
+      contextActions: [
+        FilesystemPickerNewFolderContextAction(),
+      ],
+      rootDirectory: Directory.current,
+      fsType: FilesystemType.folder,
+      fileTileSelectMode: FileTileSelectMode.wholeTile,
+    );
+
+    if (folderPath != null) {
+      await File(
+              "$folderPath${getSpliter(folderPath)}${pathController.text}.txt")
+          .writeAsString(textController.text);
+    }
+  }
+}
