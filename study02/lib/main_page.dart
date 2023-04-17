3edf6b0a610d8ef54ff20a4212b9bb5ee3ed2bd3
deveb1479@gmail.com <deveb1479@gmail.com>
2023-04-17 14:59:10 +0900

    complete to make system

--- study02/lib/main_page.dart
+++ study02/lib/main_page.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -12,14 +13,16 @@ class MainPage extends StatefulWidget {
 
 class MainPageState extends State<MainPage> {
   final int _defaultEdge = 10, _defaultBombs = 10;
-  final String _null = "", _bomb = "💥";
-  final int _empty = 0, _danger = -1;
+  final String _empty = "", _bomb = "💥";
+  final int _emptyNum = 0, _bombNum = -1;
 
+  late int _edge = _defaultEdge, _bombs = _defaultBombs;
   late List<List<ElevatedButton>> _buttonMatrix;
   late List<List<int>> _matrix;
   late List<List<bool>> _pressed;
 
   late Row _mainRow;
+  late Timer _endTimer = Timer(const Duration(seconds: 0), () {});
 
   void makeBombs(int max, int totalBombs) {
     int sum = 0;
@@ -27,8 +30,8 @@ class MainPageState extends State<MainPage> {
     while (sum < totalBombs) {
       int x = Random(DateTime.now().microsecond * 7).nextInt(max),
           y = Random(DateTime.now().microsecond * 3).nextInt(max);
-      if (_matrix[x][y] != _danger) {
-        _matrix[x][y] = _danger;
+      if (_matrix[x][y] != _bombNum) {
+        _matrix[x][y] = _bombNum;
         sum++;
       }
     }
@@ -37,25 +40,132 @@ class MainPageState extends State<MainPage> {
   void makeNums(int max) {
     for (int x = 0; x < _defaultEdge; x++) {
       for (int y = 0; y < _defaultEdge; y++) {
-        if (_matrix[x][y] == _danger) continue;
+        if (_matrix[x][y] == _bombNum) continue;
 
         int nearBombs = 0;
         if (x > 0) {
-          if (y > 0 && _matrix[x - 1][y - 1] == _danger) nearBombs++;
-          if (y < max - 1 && _matrix[x - 1][y + 1] == _danger) nearBombs++;
-          if (_matrix[x - 1][y] == _danger) nearBombs++;
+          if (y > 0 && _matrix[x - 1][y - 1] == _bombNum) nearBombs++;
+          if (y < max - 1 && _matrix[x - 1][y + 1] == _bombNum) nearBombs++;
+          if (_matrix[x - 1][y] == _bombNum) nearBombs++;
         }
         if (x < max - 1) {
-          if (y > 0 && _matrix[x + 1][y - 1] == _danger) nearBombs++;
-          if (y < max - 1 && _matrix[x + 1][y + 1] == _danger) nearBombs++;
-          if (_matrix[x + 1][y] == _danger) nearBombs++;
+          if (y > 0 && _matrix[x + 1][y - 1] == _bombNum) nearBombs++;
+          if (y < max - 1 && _matrix[x + 1][y + 1] == _bombNum) nearBombs++;
+          if (_matrix[x + 1][y] == _bombNum) nearBombs++;
         }
-        if (y > 0 && _matrix[x][y - 1] == _danger) nearBombs++;
-        if (y < max - 1 && _matrix[x][y + 1] == _danger) nearBombs++;
+        if (y > 0 && _matrix[x][y - 1] == _bombNum) nearBombs++;
+        if (y < max - 1 && _matrix[x][y + 1] == _bombNum) nearBombs++;
 
         _matrix[x][y] = nearBombs;
       }
     }
+  }
+
+  void onPressed(int x, int y, int edge, [bool check = false]) {
+    _pressed[x][y] = true;
+
+    if (check || _endTimer != null && _endTimer!.isActive) {
+      return;
+    } else if (checkWin()) {
+      showSnackBar(context, "You Win!!!");
+      _endTimer = Timer(const Duration(seconds: 5), () {
+        setState(() {
+          restart(_edge, _bombs);
+        });
+      });
+    }
+
+    if (_matrix[x][y] == 0) {
+      if (x > 0 && !_pressed[x - 1][y]) {
+        onPressed(x - 1, y, edge);
+        if (y > 0 && !_pressed[x - 1][y - 1]) onPressed(x - 1, y - 1, edge);
+        if (y < edge - 1 && !_pressed[x - 1][y + 1])
+          onPressed(x - 1, y + 1, edge);
+      }
+      if (x < edge - 1 && !_pressed[x + 1][y]) {
+        onPressed(x + 1, y, edge);
+        if (y > 0 && !_pressed[x + 1][y - 1]) onPressed(x + 1, y - 1, edge);
+        if (y < edge - 1 && !_pressed[x + 1][y + 1])
+          onPressed(x + 1, y + 1, edge);
+      }
+      if (y > 0 && !_pressed[x][y - 1]) {
+        onPressed(x, y - 1, edge);
+      }
+      if (y < edge - 1 && !_pressed[x][y + 1]) {
+        onPressed(x, y + 1, edge);
+      }
+    } else if (_matrix[x][y] == _bombNum) {
+      for (int x = 0; x < edge; x++) {
+        for (int y = 0; y < edge; y++) {
+          onPressed(x, y, edge, true);
+        }
+      }
+      showSnackBar(context, "Oops, you stepped on a mine...");
+      _endTimer = Timer(const Duration(seconds: 5), () {
+        setState(() {
+          restart(_edge, _bombs);
+        });
+      });
+    }
+  }
+
+  void restart(int edge, int bombs) {
+    _buttonMatrix = <List<ElevatedButton>>[];
+    _matrix = <List<int>>[];
+    _pressed = <List<bool>>[];
+
+    for (int x = 0; x < edge; x++) {
+      _buttonMatrix.add(<ElevatedButton>[]);
+      _matrix.add(<int>[]);
+      _pressed.add(<bool>[]);
+
+      for (int y = 0; y < edge; y++) {
+        _pressed.last.add(false);
+        _matrix.last.add(_emptyNum);
+
+        _buttonMatrix.last.add(
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                onPressed(x, y, edge);
+              });
+            },
+            style: OutlinedButton.styleFrom(
+                minimumSize: const Size(65, 65),
+                backgroundColor: _pressed[x][y]
+                    ? Colors.orange.shade500
+                    : Colors.green.shade900),
+            child: Text(_pressed[x][y]
+                ? (_matrix[x][y] == _bombNum ? _bomb : _matrix[x][y].toString())
+                : _empty),
+          ),
+        );
+      }
+    }
+
+    makeBombs(edge, bombs);
+    makeNums(edge);
+  }
+
+  void showSnackBar(BuildContext context, String text) {
+    SnackBar snackBar = SnackBar(
+      content: Text(text),
+      duration: const Duration(seconds: 5),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  bool checkWin() {
+    for (int x = 0; x < _edge; x++) {
+      for (int y = 0; y < _edge; y++) {
+        if (!_pressed[x][y] && _matrix[x][y] != _bombNum) {
+          return false;
+        }
+      }
+    }
+
+    return true;
   }
 
   MainPageState() {
@@ -70,7 +180,25 @@ class MainPageState extends State<MainPage> {
 
       for (int y = 0; y < _defaultEdge; y++) {
         _pressed.last.add(false);
-        _matrix.last.add(_empty);
+        _matrix.last.add(_emptyNum);
+
+        _buttonMatrix.last.add(
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                onPressed(x, y, _defaultEdge);
+              });
+            },
+            style: OutlinedButton.styleFrom(
+                minimumSize: const Size(65, 65),
+                backgroundColor: _pressed[x][y]
+                    ? Colors.orange.shade500
+                    : Colors.green.shade900),
+            child: Text(_pressed[x][y]
+                ? (_matrix[x][y] == _bombNum ? _bomb : _matrix[x][y].toString())
+                : _empty),
+          ),
+        );
       }
     }
 
@@ -96,18 +224,19 @@ class MainPageState extends State<MainPage> {
               _buttonMatrix[x][y] = ElevatedButton(
                 onPressed: () {
                   setState(() {
-                    _pressed[x][y] = true;
+                    onPressed(x, y, _defaultEdge);
                   });
                 },
                 style: OutlinedButton.styleFrom(
+                    minimumSize: const Size(65, 65),
                     backgroundColor: _pressed[x][y]
                         ? Colors.orange.shade500
                         : Colors.green.shade900),
                 child: Text(_pressed[x][y]
-                    ? (_matrix[x][y] == _danger
+                    ? (_matrix[x][y] == _bombNum
                         ? _bomb
                         : _matrix[x][y].toString())
-                    : _null),
+                    : _empty),
               ),
             );
       }
@@ -118,7 +247,10 @@ class MainPageState extends State<MainPage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: _mainRow,
+        child: Flexible(
+          child: _mainRow,
+          flex: 1,
+        ),
       ),
     );
   }
